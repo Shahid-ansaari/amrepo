da60f237bf8e0f6319bc64c9f94f033e7cf70b61
Shahid-ansaari <ishahidansari07@gmail.com>
2025-12-22 12:36:33 +0530

    updatein mail in mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -225,7 +225,7 @@ export default function MobileMenu({ onClose }) {
             <Phone size={20} /> +91 9266722472
           </a>
 
-          <a href="mailto:dl@amoffice.co.in" className="contact-item font-medium">
+          <a href="mailto:info@amoffices.in" className="contact-item font-medium">
             <Mail size={20} /> info@amofficegroup.in
           </a>
 
