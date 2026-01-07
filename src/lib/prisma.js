f863d46cec862afb44b5bca45c9b4536a482e73b
Shahid-ansaari <ishahidansari07@gmail.com>
2026-01-07 19:55:38 +0530

    remove /genrated and and only with p/prisma client

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -54,7 +54,8 @@
 
 
 // import { PrismaClient } from "@prisma/client";
-import { PrismaClient } from "@/generated/prisma/client";
+// import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 
